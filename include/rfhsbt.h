df9af8361b576fd2c256723737ac0868ee458e2e
r3d0t <r3d0t@proton.me>
2026-06-14 12:04:26 -0400

    editing the README file and rfhsbt.h

--- include/rfhsbt.h
+++ include/rfhsbt.h
@@ -2,6 +2,14 @@ uint8_t mac_addr[6] = MAC_ADDR;
 
 void rfhssetbtmac() {
   // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/misc_system_api.html#mac-address
+
+#if defined(ARDUINO_NANO_ESP32)
+  mac_addr[5] -= 1;
+
+#else
   mac_addr[5] -= 2;
+
+#endif
   esp_base_mac_addr_set(mac_addr);
+
 }
