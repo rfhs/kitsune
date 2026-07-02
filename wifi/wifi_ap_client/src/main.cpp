610ea35c0e19109f8dba0eab5752e8ebce943dbc
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2026-07-02 13:09:02 -0400

    fix to support all devices correctly
    also make client mode orange instead of reusing yellow

--- wifi/wifi_ap_client/src/main.cpp
+++ wifi/wifi_ap_client/src/main.cpp
@@ -11,25 +11,37 @@ void setup() {
   Serial.begin(115200);
   Serial.flush();
 
-  WiFi.persistent(false); // Stores it still in RAM
+  // Initialize Wi-Fi with default configuration to set MAC address
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  WiFi.mode(WIFI_MODE_NULL);
+  esp_wifi_init(&cfg);
+  delay(100); // adjust the esp_deep_sleep if you change this
+  // esp_wifi_start(); // esp_wifi_set_mac() only requires Wi-Fi to be *initialized*
 
   #if defined(AP)
   // Create Wi-Fi network with SSID and password
   Serial.println("Setting AP (Access Point)…");
-  mac_addr[5] -=1;
+  // Interface to set MAC address
+  wifi_interface_t interface = WIFI_IF_AP;
   #elif defined(CLIENT)
   // Connect to Wi-Fi network with SSID and password
   Serial.println("Setting WiFi Fox Client…");
+  // Interface to set MAC address
+  wifi_interface_t interface = WIFI_IF_STA;
   #endif
-  if (esp_base_mac_addr_set(mac_addr) == ESP_OK) {
+
+  // Before this desired mac address is not set
+  // Speed counts
+  uint8_t mac_addr[6] = MAC_ADDR;
+  if (esp_wifi_set_mac(interface, mac_addr) == ESP_OK) {
     Serial.println("MAC address set successfully");
   } else {
     Serial.println("Error setting MAC address");
   }
-  delay(100); // adjust the esp_deep_sleep if you change this
 
-  // Before this desired mac address is not set
-  // Speed counts
+  //  If this runs before setting mac address then
+  //  setting mac address fails on esp32c5
+  esp_wifi_set_storage(WIFI_STORAGE_RAM);
 
   rfhsledinit();
 
@@ -55,7 +67,7 @@ void setup() {
   #elif defined(CLIENT)
   WiFi.mode(WIFI_STA);
   WiFi.begin(FSSID, PSK);
-  ledcolor(0xfff700);  // YELLOW
+  ledcolor(0xff8c00);  // ORANGE
   #endif
 
   // Before this desired config is not set
