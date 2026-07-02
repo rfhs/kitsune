aec9ce1c21e21549f8ed8fdfe1332c7487bd4554
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2026-07-02 16:02:30 -0400

    check if mac address was set for bt foxes

--- include/rfhsbt.h
+++ include/rfhsbt.h
@@ -3,7 +3,7 @@
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
-void rfhssetbtmac() {
+bool rfhssetbtmac() {
   uint8_t mac_addr[6] = MAC_ADDR;
   // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/misc_system_api.html#mac-address
 #ifdef CONFIG_ESP32_UNIVERSAL_MAC_ADDRESSES
@@ -22,5 +22,17 @@ void rfhssetbtmac() {
     mac_addr[5] -= 1;
   }
 #endif
-  esp_base_mac_addr_set(mac_addr);
+#ifdef CONFIG_ESP32C5_UNIVERSAL_MAC_ADDRESSES
+#pragma message "The value of CONFIG_ESP32C5_UNIVERSAL_MAC_ADDRESSES is: " TOSTRING(CONFIG_ESP32C5_UNIVERSAL_MAC_ADDRESSES)
+  if (CONFIG_ESP32C5_UNIVERSAL_MAC_ADDRESSES == 4) {
+    mac_addr[5] -= 2;
+  } else if (CONFIG_ESP32C5_UNIVERSAL_MAC_ADDRESSES == 2) {
+    mac_addr[5] -= 1;
+  }
+#endif
+  if (esp_base_mac_addr_set(mac_addr) == ESP_OK) {
+    return true;
+  } else {
+    return false;
+  }
 }
