rfhs/kitsune

commit fad6a560a4da98db092d1afec763bf40861d014e

add 5GHz foxes

wifi/wifi_ap_client/src/conferences/9999demo.h
@@ -23,7 +23,7 @@
 #define FSSID "Demo Zuma WiFi AP Easy Fox"
 #define PSK "0123456789"
 // 1-13 permitted 0 means random
-#define CHANNEL 1
+#define CHANNEL 36
 // 0 broadcast 1 hidden
 #define SSID_HIDDEN  0
 // 1-4 permitted

wifi/wifi_ap_client/make_conference.sh
@@ -8,6 +8,7 @@ if [ -z "${1:-}" ]; then
 fi
 if [ -e "src/conferences/$(date +"%Y")${1}.h" ]; then
   printf "src/conferences/%s%s.h already exists, please remove it or pick a different name\n" "$(date +"%Y")" "${1}"
+  exit 1
 fi
 
 get_mac() {
@@ -22,4 +23,6 @@ sed -e "s#{0x00, 0x01, 0x02, 0x03, 0x04, 0x05}#$(get_mac)#" \
   -e "s#{0x00, 0x06, 0x07, 0x08, 0x09, 0x0a}#$(get_mac)#" \
   -e "s#{0x00, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}#$(get_mac)#" \
   -e "s#{0x00, 0x10, 0x11, 0x12, 0x13, 0x14}#$(get_mac)#" \
+  -e "s#{0x00, 0x15, 0x16, 0x17, 0x18, 0x19}#$(get_mac)#" \
+  -e "s#{0x00, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e}#$(get_mac)#" \
   src/conferences/9999skel.h > "src/conferences/$(date +"%Y")${1}.h"

wifi/wifi_ap_client/src/conferences/9999skel.h
@@ -55,12 +55,52 @@
 #define TIME_TO_SLEEP 45
 #define TXPOWER WIFI_POWER_7dBm
 #endif
+
+#if defined(5EASY)
+// set mac address
+#define MAC_ADDR {0x00, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+// Network config
+#define FSSID "Rex WiFi 5GHz AP Easy Fox"
+#define PSK "0123456789"
+// 1-13 permitted 0 means random
+#define CHANNEL 1
+// 0 broadcast 1 hidden
+#define SSID_HIDDEN  0
+// 1-4 permitted
+#define MAX_CLIENTS 1
+#define TIME_TO_WAKE 15
+#define TIME_TO_SLEEP 5
+// checks run during startup so we want to stay
+// running no longer than a few minutes to force
+// checks to run and led to update
+#define TXPOWER WIFI_POWER_17dBm
+#endif
+
+#if defined(5HARD)
+// set mac address
+#define MAC_ADDR {0x00, 0x10, 0x11, 0x12, 0x13, 0x14}
+// Network config
+#define FSSID "Liberty WiFi 5GHz AP Hard Fox"
+#define PSK "0123456789"
+// 1-13 permitted 0 means random
+#define CHANNEL 0
+// 0 broadcast 1 hidden
+#define SSID_HIDDEN 0
+// 1-4 permitted
+#define MAX_CLIENTS 1
+// checks run during startup so we want to stay
+// running no longer than a few minutes to force
+// checks to run and led to update
+#define TIME_TO_WAKE 30
+#define TIME_TO_SLEEP 45
+#define TXPOWER WIFI_POWER_7dBm
+#endif
 #endif
 
 #if defined(CLIENT)
 #if defined(EASY)
 // set mac address
-#define MAC_ADDR {0x00, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+#define MAC_ADDR {0x00, 0x15, 0x16, 0x17, 0x18, 0x19}
 // Network config
 #define FSSID "Tracker WiFi Client Easy Fox"
 #define PSK "0123456789"
@@ -74,7 +114,7 @@
 
 #if defined(HARD)
 // set mac address
-#define MAC_ADDR {0x00, 0x10, 0x11, 0x12, 0x13, 0x14}
+#define MAC_ADDR {0x00, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e}
 // Network config
 #define FSSID "Robo-Dog WiFi Client Hard Fox"
 #define PSK "0123456789"

commit 8824b0ac6ea19a5c239f1030020ea51b8447d2b9

set demo channel correctly
Fixes: fad6a560a4da98db092d1afec763bf40861d014e

wifi/wifi_ap_client/src/conferences/9999demo.h
@@ -23,7 +23,7 @@
 #define FSSID "Demo Zuma WiFi AP Easy Fox"
 #define PSK "0123456789"
 // 1-13 permitted 0 means random
-#define CHANNEL 36
+#define CHANNEL 1
 // 0 broadcast 1 hidden
 #define SSID_HIDDEN  0
 // 1-4 permitted