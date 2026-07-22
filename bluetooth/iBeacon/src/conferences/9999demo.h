d71b1545554c9050dcd0d9c723d941dfd4b5adf3
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2026-07-21 20:08:57 -0400

    Add demo to all the demo names

--- bluetooth/iBeacon/src/conferences/9999demo.h
+++ bluetooth/iBeacon/src/conferences/9999demo.h
@@ -1,7 +1,7 @@
 #if defined(EASY)
 // RFHS_CHALLENGE_NAME "BLE Easy Fox"
 // FOX_KEYWORDS        "Hunter should ask 'Are you the BLE Easy Fox?'"
-#define DEVICE_NAME         "Bruce BLE Easy Fox"
+#define DEVICE_NAME         "Demo Bruce BLE Easy Fox"
 #define BEACON_UUID         "A2AFA582-E4E8-4599-B28B-4E11AC6DD6E8"
 #define MANUFACTURER_ID     0x4c00
 #define MAJOR               5
@@ -12,7 +12,7 @@
 #elif defined(MEDIUM)
 // RFHS_CHALLENGE_NAME "BLE Medium Fox"
 // FOX_KEYWORDS        "Hunter should ask 'Are you the BLE Medium Fox?'"
-#define DEVICE_NAME         "Cath BLE Medium Fox"
+#define DEVICE_NAME         "Demo Cath BLE Medium Fox"
 #define BEACON_UUID         "162EEB7B-743C-47E9-9414-BBD6149B4E40"
 #define MANUFACTURER_ID     0x4c00
 #define MAJOR               5
@@ -23,7 +23,7 @@
 #elif defined(HARD)
 // RFHS_CHALLENGE_NAME "BLE Hard Fox"
 // FOX_KEYWORDS        "Hunter should ask 'Are you the BLE Hard Fox?'"
-#define DEVICE_NAME         "3Jane BLE Hard Fox"
+#define DEVICE_NAME         "Demo 3Jane BLE Hard Fox"
 #define BEACON_UUID         "DB6A0ED0-1DC3-4348-BD78-E997A0C082A3"
 #define MANUFACTURER_ID     0x4c00
 #define MAJOR               5
